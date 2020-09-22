bbd8abbd95e13c4e095bc46a60549b5fc9d76e4f
Márcio Góes <marciopgoes@hotmail.com>
2020-09-22 15:39:58 -0300

    Codigos comentados

--- chat.php
+++ chat.php
@@ -1,9 +1,10 @@
 <?php
+// inicialização do servidor WebSocket
 
 require './vendor/autoload.php';
 
 use Chat\ChatServer;
 
-$app = new Ratchet\App('localhost', 9990);
-$app->route('/chat', new ChatServer, ['*']);
-$app->run();
+$app = new Ratchet\App('localhost', 9990); // inicializa o Ratchet em localhost e porta 9990
+$app->route('/chat', new ChatServer, ['*']); // adiciona a rota do chat
+$app->run(); // começa a rodar o servidor
--- src/ChatServer.php
+++ src/ChatServer.php
@@ -1,4 +1,5 @@
 <?php
+//classe do chat extensão de MessageComponentInterface de Ratchet
 
 namespace Chat;
 
@@ -8,8 +9,8 @@ use Ratchet\ConnectionInterface;
 use Ratchet\MessageComponentInterface;
 
 final class ChatServer implements MessageComponentInterface{
-    private $clients;
-    private $colors;
+    private $clients; // objeto de clientes conectados
+    private $colors; // lista de cores para as letras iniciais
 
     public function __construct(){
         $this->clients = new SplObjectStorage();
@@ -19,22 +20,30 @@ final class ChatServer implements MessageComponentInterface{
     }
 
     public function onOpen(ConnectionInterface $conn): void{
+        // coloca um novo cliente no servidor
         $this->clients->attach($conn);
         // var_dump($conn);
     }
 
     public function onMessage(ConnectionInterface $from, $msg): void{
+
         $mensagem = json_decode($msg);
         $mensagem->date = date("H:i - d/m/y", time());
-        $mensagem->img = $mensagem->nome[0];
+        $mensagem->img = $mensagem->nome[0]; // pega a letra inicial do nome
+
+        //verifica se já existe uma cor para a letra inicial do nome
         if(!$this->$colors[$mensagem->img]){
+            // gera uma cor randomica para cada letra inicial de nome
             $rgb = array();
             foreach(array('r', 'g', 'b') as $color){
                 $rgb[$color] = mt_rand(0, 255);
             }
+            //bota a cor no array na posição da letra
             $this->$colors[$mensagem->img] = "rgb(".$rgb["r"].",".$rgb["g"].",".$rgb["b"].")";
         }
         $mensagem->color = $this->$colors[$mensagem->img];
+
+        //envia a mensagem em json para cada cliente
         foreach ($this->clients as $client) {
             $client->send(json_encode($mensagem));
         }
@@ -42,10 +51,12 @@ final class ChatServer implements MessageComponentInterface{
     }
 
     public function onClose(ConnectionInterface $conn): void{
+        // retira um cliente do servidor
         $this->clients->detach($conn);
     }
 
     public function onError(ConnectionInterface $conn, Exception $exception): void{
+        // se der erro, finaliza o canal com o cliente
         $conn->close();
     }
 }
